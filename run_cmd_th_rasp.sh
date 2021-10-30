971c0e5279bed87dadd42a2466a2c07966b32cf8
spcrcwater <89970914+spcrcwater@users.noreply.github.com>
2021-10-31 02:57:44 +0530

    grg

--- run_cmd_th_rasp.sh
+++ run_cmd_th_rasp.sh
@@ -13,7 +13,7 @@ if [[ $? -eq 0 ]]
     cicomid_new=${cicomid_new:0:7}
     if [[ "$cicomid" != "$cicomid_new" ]]
     then
-      cicomid = $cicomid_new
+      cicomid = "$cicomid_new"
       sudo systemctl restart codetest.service
     else
       echo 'No changes'
